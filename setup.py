df961cf2eee89c5b90ae4d49cce1bd21a93da528
askeing <askeing@gmail.com>
2013-05-10 17:59:34 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+# get documentation from the README
+try:
+    here = os.path.dirname(os.path.abspath(__file__))
+    description = file(os.path.join(here, 'README.md')).read()
+except (OSError, IOError):
+    description = ''
+
+# dependencies
+deps = ['marionette_client==0.5.27', ]
+
+setup(name='b2g_repl',
+      version=version,
+      description="REPL Environment for B2G",
+      long_description=description,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='mozilla',
+      author='Askeing Yen',
+      author_email='fyen@mozilla.com',
+      url='https://wiki.mozilla.org/Auto-tools/Projects/Marionette',
+      license='MPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      b2g_repl = b2g_repl.runner:main
+      """,
+      install_requires=deps,
+      )
