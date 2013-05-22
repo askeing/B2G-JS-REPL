aafe9f1fad11c8d01480866d824d351685e7a163
askeing <askeing@gmail.com>
2013-05-22 17:44:40 +0800

    Added :i for import js component. Please put js components under `b2g_js/js_component/` folder.

--- b2g_js/cmd/commands.py
+++ b2g_js/cmd/commands.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
 import codecs
 import re
 
@@ -15,6 +16,7 @@ class CmdDispatcher(object):
     _CMD_EXIT = ':q'
     _CMD_WRITE_SOURCE_TO_FILE = ':w'
     _CMD_SWITCH_SYNC_ASYNC = ':s'
+    _CMD_IMPORT_JS_COMPONENT = ':i'
 
     def __init__(self, runner, command):
         self.super_runner = runner
@@ -39,6 +41,14 @@ class CmdDispatcher(object):
         elif self.command.lower().startswith(self._CMD_SWITCH_SYNC_ASYNC):
             SwitchJSExecutionCmd(self.super_runner)
 
+        elif self.command.lower().startswith(self._CMD_IMPORT_JS_COMPONENT):
+            args = self.command.split()
+            file = None
+            if len(args) > 1:
+                file = args[1]
+            ImportJSComponentCmd(self.m, file)
+            pass
+
         else:
             print self._MSG_NO_CMD, self.command
 
@@ -54,9 +64,10 @@ class PrintUsageCmd(object):
         print output_head_format.format('#Commands', '#Description')
         print output_format.format(CmdDispatcher._CMD_HELP, '', 'Print usage.')
         print output_format.format(CmdDispatcher._CMD_EXIT, '', 'Exit.')
-        print output_format.format(CmdDispatcher._CMD_WRITE_SOURCE_TO_FILE, '<FILE>', 'Print page source to file. Default FILE is output.txt.')
-        #print output_format.format(':e', '<FILE>', 'Read javascript from file. Do nothing if no FILE.')
+        print output_format.format(CmdDispatcher._CMD_WRITE_SOURCE_TO_FILE, '<FILE>', 'Print page source to file. Default <FILE> is output.txt.')
+        #print output_format.format(':e', '<FILE>', 'Read javascript from file. Do nothing if no <FILE>.')
         print output_format.format(CmdDispatcher._CMD_SWITCH_SYNC_ASYNC, '', 'Switch sync/async javascript execution.')
+        print output_format.format(CmdDispatcher._CMD_IMPORT_JS_COMPONENT, '<FILE>', 'Import javascript from file. List all components if no <FILE>.')
         print output_format.format('', 'exit', 'Exit.')
 
 
@@ -80,3 +91,30 @@ class WritePageSourceToFileCmd(object):
         f = codecs.open(self.dest, 'w', 'utf8')
         f.write(self.m.page_source)
         f.close()
+
+
+class ImportJSComponentCmd(object):
+
+    def __init__(self, marionette, file=None):
+        self.js_dir = './b2g_js/js_component'
+        self.m = marionette
+        self.file = file
+        self()
+
+    def __call__(self):
+        if os.path.exists(self.js_dir) == True:
+            # list all js components
+            if self.file == None:
+                js_files = [f for f in os.listdir(self.js_dir) if os.path.isfile(os.path.join(self.js_dir, f))]
+                for js_file in js_files:
+                    print js_file
+            # check the file exists, then import it
+            else:
+                js = os.path.abspath(os.path.join(self.js_dir, self.file))
+                if os.path.exists(js) == True and os.path.isfile(js) == True:
+                    print 'Imported:', js
+                    self.m.import_script(js)
+                else:
+                    print 'No JS components "%s" under "%s" folder.' % (self.file, self.js_dir)
+        else:
+            print 'No JS components under "%s" folder.' % self.js_dir
