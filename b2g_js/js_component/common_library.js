a93cc35d6fc4b2d50f77fde5655d96c5f0f31a1e
Walter C <wachen@mozilla.com>
2013-07-08 17:39:11 +0800

    Adding a common library js for shared javascript library.

---
+++ b2g_js/js_component/common_library.js
@@ -0,0 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+'use strict';
+
+// This function is made to be like dir() in Python
+function dir(object) {
+  var list = [];
+  for(var s in object) {
+    list.push(s);
+  }
+  list.sort();
+  return list;
+}
